560f7bdcb7cb83f665955d41a901bcce853502f3
Csengeri Marcell <csengeri.marcell@petrik.hu>
2025-03-02 21:42:31 +0100

    3. állat hozzáadva

---
+++ Allatkert/Szarvas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Allatkert
+{
+    internal class Szarvas
+    {
+        private string nev;
+        private int kor;
+        private int sebesseg;
+        private int szelidseg;
+
+        public string Nev { get => nev; set => nev = value; }
+        public int Kor { get => kor; set => kor = value; }
+        public int Sebesseg { get => sebesseg; set => sebesseg = value; }
+        public int Szelidseg { get => szelidseg; set => szelidseg = value; }
+
+        public Szarvas(string nev, int kor, int sebesseg, int szelidseg)
+        {
+            this.nev = nev;
+            this.kor = kor;
+            this.sebesseg = sebesseg;
+            this.szelidseg = szelidseg;
+        }
+    }
+}
