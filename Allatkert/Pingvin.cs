5ea8dd8c755b1c7144529c81739b8cdabbf9b126
Csengeri Marcell <csengeri.marcell@petrik.hu>
2025-03-02 21:22:48 +0100

    Add project files.

---
+++ Allatkert/Pingvin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Allatkert
+{
+    internal class Pingvin
+    {
+        private string nev;
+        private int kor;
+        private int sebesseg;
+        private int uszasiSebesseg;
+
+        public string Nev { get => nev; set => nev = value; }
+        public int Kor { get => kor; set => kor = value; }
+        public int Sebesseg { get => sebesseg; set => sebesseg = value; }
+        public int UszasiSebesseg { get => uszasiSebesseg; set => uszasiSebesseg = value; }
+
+        public Pingvin(string nev, int kor, int sebesseg, int uszasiSebesseg)
+        {
+            this.nev = nev;
+            this.kor = kor;
+            this.sebesseg = sebesseg;
+            this.uszasiSebesseg = uszasiSebesseg;
+        }
+
+
+    }
+}
---
+++ Allatkert/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Allatkert
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Üdvözölünk az állatkerben!");
+
+        }
+    }
+}
