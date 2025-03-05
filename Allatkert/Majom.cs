be6e9428deabf4200ac1f83c315a28088af9a91e
Csengeri Marcell <csengeri.marcell@petrik.hu>
2025-03-05 21:29:11 +0100

    5. állat hozzáadva

---
+++ Allatkert/Majom.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Allatkert
+{
+    internal class Majom
+    {
+        private string nev;
+        private int kor;
+        private int sebesseg;
+        private int intelligencia;
+
+        public string Nev { get => nev; set => nev = value; }
+        public int Kor { get => kor; set => kor = value; }
+        public int Sebesseg { get => sebesseg; set => sebesseg = value; }
+        public int Intelligencia { get => intelligencia; set => intelligencia = value; }
+
+        public Majom(string nev, int kor, int sebesseg, int intelligencia)
+        {
+            this.nev = nev;
+            this.kor = kor;
+            this.sebesseg = sebesseg;
+            this.intelligencia = intelligencia;
+        }
+    }
+}
