7dece4cf160310edb14cdfa02a480d7b287c362c
Csengeri Marcell <csengeri.marcell@petrik.hu>
2025-03-06 19:03:03 +0100

    Állatkert nagyságának kiírása kész

--- Allatkert/Program.cs
+++ Allatkert/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            int allatokSzama = random.Next(5, 16);
             Console.WriteLine("Üdvözölünk az állatkerben!");
+
+            Console.WriteLine("Állatkert telítettsége:");
+            Console.WriteLine("-----------------");
+            Console.Write("|");
+            Console.ForegroundColor = ConsoleColor.Green;
+            for (int i = 0; i < allatokSzama; i++)
+            {
+                Console.Write("■");
+            }
+            for (int i = 0; i < 15-allatokSzama; i++)
+            {
+                Console.Write(" ");
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("|");
+            Console.WriteLine("-----------------");
+
+
 
         }
     }
