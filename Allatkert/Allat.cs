2d412c90ba6ede7597a93748a9ede29c071a59bc
Csengeri Marcell <csengeri.marcell@petrik.hu>
2025-03-06 22:57:39 +0100

    Versenyzés funkció kész

---
+++ Allatkert/Allat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Allatkert
+{
+    internal class Allat
+    {
+        private string nev;
+        private int sebesseg;
+
+        public string Nev { get => nev; set => nev = value; }
+        public int Sebesseg { get => sebesseg; set => sebesseg = value; }
+
+        public Allat(string nev, int sebesseg)
+        {
+            this.nev = nev;
+            this.sebesseg = sebesseg;
+        }
+    }
+}
--- Allatkert/Program.cs
+++ Allatkert/Program.cs
@@ -2,6 +2,8 @@
 {
     internal class Program
     {
+        static List<Allat> allatok = new List<Allat>();
+
         static void Main(string[] args)
         {
             Random random = new Random();
@@ -16,13 +18,63 @@
             {
                 Console.Write("■");
             }
-            for (int i = 0; i < 15-allatokSzama; i++)
+            for (int i = 0; i < 15 - allatokSzama; i++)
             {
                 Console.Write(" ");
             }
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("|");
             Console.WriteLine("-----------------");
+
+            Pingvin a1 = new Pingvin("Róbert a Pingvin", 3, 2, 6);
+            SarkiRoka a2 = new SarkiRoka("Károly A Róka", 2, 6, 11);
+            Szarvas a3 = new Szarvas("Béla a Szarvas", 5, 3, 8);
+            Zsiraf a4 = new Zsiraf("Paula a Zsiráf", 7, 4, 9);
+            Majom a5 = new Majom("Géza a Majom", 4, 5, 7);
+
+            allatok.Add(new Allat(a1.Nev, a1.Sebesseg));
+            allatok.Add(new Allat(a2.Nev, a2.Sebesseg));
+            allatok.Add(new Allat(a3.Nev, a3.Sebesseg));
+            allatok.Add(new Allat(a4.Nev, a4.Sebesseg));
+            allatok.Add(new Allat(a5.Nev, a5.Sebesseg));
+
+            Console.Write("Versenyezzenek az állatok? (igen/nem) ");
+            string valasz = Console.ReadLine();
+            if (valasz == "igen")
+            {
+                Versenyzes();
+            } 
+        }
+
+        static void Versenyzes()
+        {
+            int versenyVege = 50;
+            int legelso = 0;
+            int lepes = 0;
+
+            while (versenyVege > legelso)
+            {
+                Thread.Sleep(1000);
+                Console.Clear();
+                foreach (var allat in allatok)
+                {
+                    int allatHelyzete = allat.Sebesseg * lepes;
+                    if (allatHelyzete > legelso)
+                    {
+                        legelso = allatHelyzete;
+                    }
+
+                    Console.Write($"{allat.Nev} :");
+                    for (int i = 0; i < (allatHelyzete); i++)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write("O");
+                    Console.WriteLine();
+                }
+
+                lepes++;
+            }
 
 
 
